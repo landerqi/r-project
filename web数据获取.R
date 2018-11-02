f9bf834dfb1c60f6f2a5b535e434e071b98fa852
landerqi <landerqi@gmail.com>
2018-11-02 18:40:42 +0800

    feat:get web data by api

--- web数据获取.R
+++ web数据获取.R
@@ -1,16 +1,30 @@
 # web数据获取
 # 结构化的网页数据抓取
 library(XML)
-#webpage <- 'http://data.earthquake.cn/datashare/globeEarthquake_csn.html'
-webpage <- 'http://www.cea.gov.cn/publish/dizhenj/464/479/index.html'
-webpage
+webpage <- 'http://landerqi.com'
+# webpage <- 'https://bulma.io/documentation/elements/table/'
 tables <- readHTMLTable(webpage, stringsAsFactors = FALSE)
 tables
-raw <- tables[[6]]
-raw
+raw <- tables[[2]]
 data <- raw[, 3:5]
-data
+data <- data[-1, ]
+names(data) <- c('lan', 'lon', 'deep')
+sapply(data, class)
+data$deep <- as.numeric(data$deep)
+summary(data$deep)
+raw[which.max(data$deep), ]
+data <- transform(data, lan = as.numeric(lan), lon = as.numeric(lon))
+hist(data$lan, 40)
+hist(data$lon, 40)
 
 # 非结构化网页数据抓取
+### xpath使用
+library(XML)
+url <- 'http://landerqi.com/'
+landerqi <- htmlParse(url, encoding = 'UTF-8')
+xpath <- '//*[@id="wrapper"]/article[1]/div/div'
+nodes <- getNodeSet(landerqi, xpath)
+names <- sapply(nodes, xmlValue)
 
 # Api
+# https://api.douban.com/v2/movie/subject/5323968
